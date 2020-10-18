9415d18c4c4596614f1cf7c1dfd52ae9ae3d36e7
vdonoladev <victordonolaf@gmail.com>
2020-10-18 17:56:34 -0300

    Updates

--- etapas.js
+++ etapas.js
@@ -1,50 +1,46 @@
 let etapas = [
-	{
-		titulo: 'VEREADOR',
-		numeros: 5,
-		candidatos: [
-		{
-			numero: '17874',
-			nome: 'Fulana de Tal',
-			partido: 'ABC',
-			fotos:[
-				{url:'17874.jpg', legenda: 'Vereadora'}
-			]
-		},
-		{
-			numero: '13478',
-			nome: 'Beltrano de Tal',
-			partido: 'DEFG',
-			fotos:[
-				{url:'13478.jpg', legenda: 'Vereador'}
-			]
-		},
-		]
-	},
-	{
-		titulo: 'PREFEITO',
-		numeros: 2,
-		candidatos: [
-			{
-				numero: '17',
-				nome: 'Ciclano',
-				partido: 'ABC',
-				vice: 'Cic',
-				fotos:[
-					{url:'17.jpg', legenda: 'Prefeito'},
-					{url:'17-vice.jpg', legenda: 'Vice-Prefeita', small: true}
-				]
-			},
-			{
-				numero: '13',
-				nome: 'Zulano',
-				partido: 'QWERTY',
-				vice: 'Zul',
-				fotos:[
-					{url:'13.jpg', legenda: 'Prefeito'},
-					{url:'13-vice.jpg', legenda: 'Vice-Prefeita', small: true}
-				]
-			},
-		]
-	}
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "17874",
+        nome: "Fulana de Tal",
+        partido: "ABC",
+        fotos: [{ url: "17874.jpg", legenda: "Vereadora" }],
+      },
+      {
+        numero: "13478",
+        nome: "Beltrano de Tal",
+        partido: "DEFG",
+        fotos: [{ url: "13478.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "17",
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          { url: "17.jpg", legenda: "Prefeito" },
+          { url: "17-vice.jpg", legenda: "Vice-Prefeita", small: true },
+        ],
+      },
+      {
+        numero: "13",
+        nome: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "13.jpg", legenda: "Prefeito" },
+          { url: "13-vice.jpg", legenda: "Vice-Prefeita", small: true },
+        ],
+      },
+    ],
+  },
 ];
